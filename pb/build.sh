b5194a3235aa10a87dc7b588d7499f87722321eb
Andy Kreling <krelinga@gmail.com>
2024-05-29 03:55:24 +0000

    first draft of proto files.

---
+++ pb/build.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+set -e
+
+rm -f *.pb.go
+protoc \
+    -I=. \
+    --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    *.proto
