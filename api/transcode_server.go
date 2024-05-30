3d877dca0565123ad9917f58838455573bb6f1c0
Andy Kreling <krelinga@gmail.com>
2024-05-30 22:19:40 +0000

    plumbing to start a workflow run on the API server.

--- api/transcode_server.go
+++ api/transcode_server.go
@@ -2,8 +2,11 @@ package main
 
 import (
     "context"
+    "fmt"
 
+    "github.com/krelinga/transcode-service/common"
     "github.com/krelinga/transcode-service/pb"
+    "github.com/krelinga/transcode-service/workflow"
     "go.temporal.io/sdk/client"
 )
 
@@ -18,8 +21,30 @@ func NewTranscodeServer(temporalC client.Client) *TranscodeServer {
     }
 }
 
-func (_ *TranscodeServer) BeginOneFile(ctx context.Context, req *pb.BeginOneFileRequest) (*pb.BeginOneFileReply, error) {
-    return &pb.BeginOneFileReply{}, nil
+func tcOneFileWorkflowID(inPath, outPath string) string {
+    return fmt.Sprintf("%s to %s", inPath, outPath)
+}
+
+func (tcs *TranscodeServer) BeginOneFile(ctx context.Context, req *pb.BeginOneFileRequest) (*pb.BeginOneFileReply, error) {
+    options := client.StartWorkflowOptions{
+        ID: tcOneFileWorkflowID(req.InPath, req.OutPath),
+        TaskQueue: common.TaskQueue,
+    }
+    workflowReq := &workflow.TranscodeOneFileRequest{
+        InPath: req.InPath,
+        OutPath: req.OutPath,
+    }
+    workflowRun, err := tcs.temporalC.ExecuteWorkflow(ctx, options, workflow.TranscodeOneFile, workflowReq)
+    if err != nil {
+        return nil, err
+    }
+    return &pb.BeginOneFileReply{
+        Key: &pb.OneFileKey{
+            InPath: req.InPath,
+            OutPath: req.OutPath,
+            Instance: workflowRun.GetRunID(),
+        },
+    }, nil
 }
 
 func (_ *TranscodeServer) CheckOneFile(ctx context.Context, req *pb.CheckOneFileRequest) (*pb.CheckOneFileReply, error) {
