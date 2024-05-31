be4907c351c0f4033e2a14939ccd87b4a53a6376
Andy Kreling <krelinga@gmail.com>
2024-05-31 01:33:31 +0000

    connect to temporal client in api server.

--- api/main.go
+++ api/main.go
@@ -4,14 +4,42 @@ import (
     "fmt"
     "log"
     "net"
+    "os"
 
     "github.com/krelinga/transcode-service/pb"
     "go.temporal.io/sdk/client"
     "google.golang.org/grpc"
 )
 
+func temporalHostPort() (string, error) {
+    get := func(k string) (string, error) {
+        v := os.Getenv(k)
+        if len(v) == 0 {
+            return "", fmt.Errorf("could not read env var %s", k)
+        }
+        return v, nil
+    }
+
+    host, err := get("temporal_host")
+    if err != nil {
+        return "", err
+    }
+    port, err := get("temporal_port")
+    if err != nil {
+        return "", err
+    }
+
+    return fmt.Sprintf("%s:%s", host, port), nil
+}
+
 func mainOrError() error {
-    temporalC, err := client.Dial(client.Options{})
+    hp, err := temporalHostPort()
+    if err != nil {
+        return err
+    }
+    temporalC, err := client.Dial(client.Options{
+        HostPort: hp,
+    })
     if err != nil {
         return fmt.Errorf("Could not create temporal client: %w", err)
     }
